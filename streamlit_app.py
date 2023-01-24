0a1911d28a3acdbfa1b7425f3aa5d54ffb96ff31
Fumio Takeuchi <52876316+ftaked@users.noreply.github.com>
2023-01-25 01:47:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parent new health dinner')
+streamlit.header('Breakefast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinatch & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-range Egg')
