fcb878378463c779057c24309c4ffe5b8ceefb69
Fumio Takeuchi <52876316+ftaked@users.noreply.github.com>
2023-01-29 22:28:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,8 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
 
-
+except URLError as e:
+  streamlit.error(e)
 
 # これより下のセクションには実行させない
 streamlit.stop()
